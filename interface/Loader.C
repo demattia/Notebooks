7d25f8774549fbc8585cfb234c47e7360ca866e7
demattia <marco.de.mattia@cern.ch>
2014-01-18 11:41:57 -0600

    Added new noteboks

---
+++ interface/Loader.C
@@ -0,0 +1,22 @@
+#include <vector>
+#include <map>
+#include <string>
+#include "Candidates.h"
+#include "TreeDipseudoLeptonCandidate.h"
+// #include "GenEventProperties.h"
+// #include "PseudoLepton.h"
+#include "TreeLepton.h"
+#include "TreePhoton.h"
+#include "TreeDiphotonCandidate.h"
+#ifdef __CINT__
+#pragma link C++ class Candidates+;
+#pragma link C++ class std::vector<Candidates>+;
+#pragma link C++ class TreeDipseudoLeptonCandidate+;
+#pragma link C++ class std::vector<TreeDipseudoLeptonCandidate>+;
+#pragma link C++ class TreeLepton+;
+#pragma link C++ class std::vector<TreeLepton>+;
+#pragma link C++ class TreePhoton+;
+#pragma link C++ class std::vector<TreePhoton>+;
+#pragma link C++ class TreeDiphotonCandidate+;
+#pragma link C++ class std::vector<TreeDiphotonCandidate>+;
+#endif
